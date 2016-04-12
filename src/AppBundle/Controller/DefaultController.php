1fb3f29244b19926f058b00112013bc2ecf5179d
thomas-lienard <t.lienard92@gmail.com>
2016-04-12 17:45:58 +0200

    add bootstrap

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/nnn", name="homepage")
      */
     public function indexAction(Request $request)
     {
