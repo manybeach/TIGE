6fb0768126e5d93d1e4f88f0466ae39b700c4cc2
thomas-lienard <t.lienard92@gmail.com>
2016-04-16 18:11:14 +0200

    statsBundle par thomas

--- src/StatsBundle/Controller/DefaultController.php
+++ src/StatsBundle/Controller/DefaultController.php
@@ -2,7 +2,10 @@
 
 namespace StatsBundle\Controller;
 
+use Ob\HighchartsBundle\Highcharts\Highchart;
+use Ob\HighchartsBundle\Highcharts\ChartInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 
 class DefaultController extends Controller
 {
@@ -58,14 +61,57 @@ class DefaultController extends Controller
                 'totalSessionsPlayed' => $arrayStats["stats"]["totalSessionsPlayed"],
                 'totalSessionsLost' => $arrayStats["stats"]["totalSessionsLost"],
                 'totalSessionsWon' => $arrayStats["stats"]["totalSessionsWon"],
+                'totalChampionKills' => $arrayStats["stats"]["totalChampionKills"],
+                'totalMinionKills' => $arrayStats["stats"]["totalMinionKills"],
+                'totalGoldEarned' => $arrayStats["stats"]["totalGoldEarned"],
+                'totalAssists' => $arrayStats["stats"]["totalAssists"],
+                'totalDeathsPerSession' => $arrayStats["stats"]["totalDeathsPerSession"],
+                'totalDamageDealt' => $arrayStats["stats"]["totalDamageDealt"],
+                'totalDamageTaken' => $arrayStats["stats"]["totalDamageTaken"],
+
+
             );
             array_push($arrayStatsRanked, $arrayDataBySum);
         }
 
-    var_dump($arrayStatsRanked);
+
         return true;
     }
 
+    public function victoryChartAction(){
+                $sellsHistory = array(
+            array(
+                 "name" => "Bénéfices total",
+                 "data" => array(9.1, 10.3, 6.5, 12.2, 5.3, 9.1, 11.1)
+            ),
+            array(
+                 "name" => "Bénéfices pour la France",
+                 "data" => array(6.6, 8.2, 0.76, 4.6, 2.1, 4.1, 3.9)
+            ),
+
+        );
+
+        $dates = array(
+            "21/06", "22/06", "23/06", "24/06", "25/06", "26/06", "27/06"
+        );
+
+        $ob = new Highchart();
+        // ID de l'élement de DOM que vous utilisez comme conteneur
+        $ob->chart->renderTo('barchart');
+        $ob->title->text('Bénéfices du 21/06/2013 au 27/06/2013');
+        $ob->chart->type('column');
+
+        $ob->yAxis->title(array('text' => "Bénéfices (millions d'euros)"));
+
+        $ob->xAxis->title(array('text' => "Date du jours"));
+        $ob->xAxis->categories($dates);
+
+        $ob->series($sellsHistory);
+
+        return $this->render('StatsBundle:Default:index.html.twig', array(
+            'barchart' => $ob
+        ));
+    }
 
 
 
