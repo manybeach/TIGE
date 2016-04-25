f71b350c47201cfd144ffca7f38d1e1a8ded164f
tony <massontony@live.fr>
2016-04-25 16:45:36 +0200

    img

--- src/StatsBundle/Controller/DefaultController.php
+++ src/StatsBundle/Controller/DefaultController.php
@@ -14,48 +14,70 @@ class DefaultController extends Controller
         return $this->render('StatsBundle:Default:index.html.twig');
     }
 
+    public function sellsHistoryAction()
+    {
+
+        $arrayStatsChampions = $this->getStatsDataAction();
+
+        $ob = new Highchart();
+        $ob->chart->renderTo('piechart');
+        $ob->title->text('Nombre de parties jouées par champion');
+        $ob->plotOptions->pie(array(
+            'allowPointSelect' => true,
+            'cursor' => 'pointer',
+            'dataLabels' => array('enabled' => true),
+            'showInLegend' => false
+        ));
+        $data = array();
+        foreach($arrayStatsChampions as $hero)
+        {
+            $heroName = $this->getNameChampById($hero["id"]);
+            if($heroName != false)
+                array_push($data,array($heroName, $hero["totalSessionsPlayed"]));
+        }
+        $ob->series(array(array('type' => 'pie', 'name' => 'Browser share', 'data' => $data)));
+
+        return $this->render('StatsBundle:Default:index.html.twig', array(
+            'piechart' => $ob
+        ));
+    }
+
 
     public function getStatsDataAction()
     {
         $arrayStatsSummary = array();
-        $arrayStatsRanked =array();
+        $arrayStatsRanked = array();
         $server = 'euw';
         $summonerName = strtolower('BabyKywa');
 
         // on recupere le summonerID à partir du summonerName
-        $summonerId = $this->getSummonerId($summonerName,$server);
+        $summonerId = $this->getSummonerId($summonerName, $server);
 
-        // on recupere le résumé des stats du joueur à partir de son ID
-        $resultStatsSummary = $this->getStatsSummary($summonerId, $server);
+        /* // on recupere le résumé des stats du joueur à partir de son ID
+         $resultStatsSummary = $this->getStatsSummary($summonerId, $server);
 
+         foreach ($resultStatsSummary["playerStatSummaries"] as $arrayStats) {
 
-        foreach ($resultStatsSummary["playerStatSummaries"] as $arrayStats) {
-
-            if (empty($arrayStats["wins"])) {
-                $arrayStats["wins"] = 0;
-            }
-            if (empty($arrayStats["losses"])) {
-                $arrayStats["losses"] = 0;
-            }
-            $arrayDataBySum = array(
-                'wins' => $arrayStats["wins"],
-                'losses' => $arrayStats["losses"],
-                'playerStatSummaryType' => $arrayStats["playerStatSummaryType"]
-            );
-            array_push($arrayStatsSummary, $arrayDataBySum);
-        }
+             if (empty($arrayStats["wins"])) {
+                 $arrayStats["wins"] = 0;
+             }
+             if (empty($arrayStats["losses"])) {
+                 $arrayStats["losses"] = 0;
+             }
+             $arrayDataBySum = array(
+                 'wins' => $arrayStats["wins"],
+                 'losses' => $arrayStats["losses"],
+                 'playerStatSummaryType' => $arrayStats["playerStatSummaryType"]
+             );
+             array_push($arrayStatsSummary, $arrayDataBySum);
+         }*/
 
 
         // on recupere les stats ranked du joueur à partir de son ID
-        $resultStatsRanked= $this->getStatsRanked($summonerId, $server);
+        $resultStatsRanked = $this->getStatsRanked($summonerId, $server);
+
         foreach ($resultStatsRanked["champions"] as $arrayStats) {
 
-            if (empty($arrayStats["wins"])) {
-                $arrayStats["wins"] = 0;
-            }
-            if (empty($arrayStats["losses"])) {
-                $arrayStats["losses"] = 0;
-            }
             $arrayDataBySum = array(
                 'id' => $arrayStats["id"],
                 'totalSessionsPlayed' => $arrayStats["stats"]["totalSessionsPlayed"],
@@ -73,46 +95,8 @@ class DefaultController extends Controller
             );
             array_push($arrayStatsRanked, $arrayDataBySum);
         }
-
-
-        return true;
+        return $arrayStatsRanked;
     }
-
-    public function victoryChartAction(){
-                $sellsHistory = array(
-            array(
-                 "name" => "Bénéfices total",
-                 "data" => array(9.1, 10.3, 6.5, 12.2, 5.3, 9.1, 11.1)
-            ),
-            array(
-                 "name" => "Bénéfices pour la France",
-                 "data" => array(6.6, 8.2, 0.76, 4.6, 2.1, 4.1, 3.9)
-            ),
-
-        );
-
-        $dates = array(
-            "21/06", "22/06", "23/06", "24/06", "25/06", "26/06", "27/06"
-        );
-
-        $ob = new Highchart();
-        // ID de l'élement de DOM que vous utilisez comme conteneur
-        $ob->chart->renderTo('barchart');
-        $ob->title->text('Bénéfices du 21/06/2013 au 27/06/2013');
-        $ob->chart->type('column');
-
-        $ob->yAxis->title(array('text' => "Bénéfices (millions d'euros)"));
-
-        $ob->xAxis->title(array('text' => "Date du jours"));
-        $ob->xAxis->categories($dates);
-
-        $ob->series($sellsHistory);
-
-        return $this->render('StatsBundle:Default:index.html.twig', array(
-            'barchart' => $ob
-        ));
-    }
-
 
 
     /**
@@ -125,8 +109,9 @@ class DefaultController extends Controller
     function getSummonerId(
         $summonerName,
         $server
-    ) {
-        $url = 'https://'.$server.'.api.pvp.net/api/lol/'.$server.'/v1.4/summoner/by-name/'.$summonerName.'?api_key=0610f47d-dba7-46ff-84c7-fc9eeee8b788';
+    )
+    {
+        $url = 'https://' . $server . '.api.pvp.net/api/lol/' . $server . '/v1.4/summoner/by-name/' . $summonerName . '?api_key=0610f47d-dba7-46ff-84c7-fc9eeee8b788';
         $resultJson = file_get_contents($url);
         $result = json_decode($resultJson, true);
         $summonerId = $result[$summonerName]['id'];
@@ -138,19 +123,37 @@ class DefaultController extends Controller
     function getStatsSummary(
         $summonerId,
         $server
-    ) {
-        $url = 'https://'.$server.'.api.pvp.net/api/lol/'.$server.'/v1.3/stats/by-summoner/'.$summonerId.'/summary?api_key=0610f47d-dba7-46ff-84c7-fc9eeee8b788';
+    )
+    {
+        $url = 'https://' . $server . '.api.pvp.net/api/lol/' . $server . '/v1.3/stats/by-summoner/' . $summonerId . '/summary?api_key=0610f47d-dba7-46ff-84c7-fc9eeee8b788';
         $resultJson = file_get_contents($url);
         $result = json_decode($resultJson, true);
 
         return $result;
     }
 
-    private function getStatsRanked($summonerId, $server){
-        $url = 'https://'.$server.'.api.pvp.net/api/lol/'.$server.'/v1.3/stats/by-summoner/'.$summonerId.'/ranked?api_key=0610f47d-dba7-46ff-84c7-fc9eeee8b788';
+    private function getStatsRanked($summonerId, $server)
+    {
+        $url = 'https://' . $server . '.api.pvp.net/api/lol/' . $server . '/v1.3/stats/by-summoner/' . $summonerId . '/ranked?api_key=0610f47d-dba7-46ff-84c7-fc9eeee8b788';
         $resultJson = file_get_contents($url);
         $result = json_decode($resultJson, true);
 
         return $result;
+    }
+
+    private function getNameChampById($id)
+    {
+        $result = false;
+        if(!empty($id))
+        {
+            $url = "https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/$id?api_key=0610f47d-dba7-46ff-84c7-fc9eeee8b788";
+            $resultJson = file_get_contents($url);
+            $result = json_decode($resultJson, true);
+            $result = $result["name"];
+
+        }
+
+        return $result;
+
     }
 }
