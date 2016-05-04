a20df32aab07f96ba773699940a2bc016a1e4bb9
thomas-lienard <t.lienard92@gmail.com>
2016-05-04 16:11:19 +0200

    update style

--- src/AppBundle/Form/RegistrationType.php
+++ src/AppBundle/Form/RegistrationType.php
@@ -13,34 +13,65 @@ class RegistrationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array(
-                'label' => 'Email', 'translation_domain' => 'FOSUserBundle',
-                'attr' => array('class' => 'form-control input-md',
-                                'placeholder' => 'Entrez votre email'
+            ->add(
+                'email',
+                LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'),
+                array(
+                    'label' => 'Email',
+                    'translation_domain' => 'FOSUserBundle',
+                    'attr' => array(
+                        'class' => 'form-control input-md',
+                        'placeholder' => 'Entrez votre email',
+                    ),
                 )
             )
-            )
-            ->add('address', null, array('label' => 'Adresse', 'translation_domain' => 'FOSUserBundle','attr' => array(
-                'class' => 'form-control input-md',
-                'placeholder' => 'Entrez votre adresse')
-            ))
-            ->add('username', null, array('label' => 'Username', 'translation_domain' => 'FOSUserBundle','attr' => array(
-                            'class' => 'form-control input-md',
-                           'placeholder' => 'Entrez votre username')
-            ))
-
-            ->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
-                'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
-                'options' => array('translation_domain' => 'FOSUserBundle'),
-                'first_options' => array('label' => 'Mot de passe', 'attr' => array(
-                                            'class' => 'form-control input-md',
-                                            'placeholder' => 'Entrez votre mot de passe')),
-                'second_options' => array('label' => 'Confirmation du mot de passe' , 'attr' => array(
+            ->add(
+                'address',
+                null,
+                array(
+                    'label' => 'Adresse',
+                    'translation_domain' => 'FOSUserBundle',
+                    'attr' => array(
                         'class' => 'form-control input-md',
-                        'placeholder' => 'Confirmez votre mot de passe')),
-                'invalid_message' => 'Mot de passe invalide'
-            ))
-        ;
+                        'placeholder' => 'Entrez votre adresse',
+                    ),
+                )
+            )
+            ->add(
+                'username',
+                null,
+                array(
+                    'label' => 'Username',
+                    'translation_domain' => 'FOSUserBundle',
+                    'attr' => array(
+                        'class' => 'form-control input-md',
+                        'placeholder' => 'Entrez votre username',
+                    ),
+                )
+            )
+            ->add(
+                'plainPassword',
+                LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'),
+                array(
+                    'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
+                    'options' => array('translation_domain' => 'FOSUserBundle'),
+                    'first_options' => array(
+                        'label' => 'Mot de passe',
+                        'attr' => array(
+                            'class' => 'form-control input-md',
+                            'placeholder' => 'Entrez votre mot de passe',
+                        ),
+                    ),
+                    'second_options' => array(
+                        'label' => 'Confirmation du mot de passe',
+                        'attr' => array(
+                            'class' => 'form-control input-md',
+                            'placeholder' => 'Confirmez votre mot de passe',
+                        ),
+                    ),
+                    'invalid_message' => 'Mot de passe invalide',
+                )
+            );
     }
 
     public function getParent()
